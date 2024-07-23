e0379f958009a49f157f1f3a9c90937195c62281
Niamh Carter <niamh.carter13@gmail.com>
2024-07-23 19:34:14 +0100

    Added social icons to the navigation bar

--- Portfolio/src/Navigation.js
+++ Portfolio/src/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Routes, Route, Outlet } from "react-router-dom";
 import { Navbar, Nav } from 'react-bootstrap';
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import Home from './BasePages/Home.js';
 import AllProjects from './BasePages/AllProjects.js';
 import About from './BasePages/About.js';
@@ -9,7 +10,7 @@ import Contact from './BasePages/Contact.js';
 export default function Navigation() {
     return (
         <Router>
-            <Navbar bg='light'>
+            <Navbar bg='light' style={{paddingRight: 25, paddingLeft: 25}}>
                 <Navbar.Brand>
                     <Nav.Link as={Link} to="/">
                         <h1>Niamh Carter</h1>
@@ -26,6 +27,14 @@ export default function Navigation() {
                         </Nav.Link>
                         <Nav.Link as={Link} to='contact'>
                             Contact
+                        </Nav.Link>
+                    </Nav>
+                    <Nav className='ms-auto' style={{paddingRight: 30}}>
+                        <Nav.Link href='https://github.com/NSCarter' target='_blank'>
+                            <FaGithub size={30} />
+                        </Nav.Link>
+                        <Nav.Link href='https://www.linkedin.com/in/niamh-carter/' target='_blank'>
+                            <FaLinkedin size={30} />
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
