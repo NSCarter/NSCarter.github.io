56edb023d38d30fe7c7880849799ca421564adf3
Niamh Carter <niamh.carter13@gmail.com>
2025-01-23 18:08:03 +0000

    Added project screenshots to the hero section

--- Portfolio/src/Home/HeroSection.js
+++ Portfolio/src/Home/HeroSection.js
@@ -1,10 +1,16 @@
 import './HeroSection.css';
+import Stack from 'react-bootstrap/Stack';
 
 export default function HeroSection() {
     return (
-        <div id="title">
-            <h1 id="bigHeader">Hi, I'm Niamh - A Software Developer</h1>
-            <h3>Building .NET and native mobile applications</h3>
+        <div>
+            <Stack direction="horizontal">
+            <div id="title">
+                <h1 id="bigHeader">Hi, I'm Niamh - A Software Developer</h1>
+                <h3>Building .NET and native mobile applications</h3>
+            </div>
+            <img src="HeroSectionImage.png" />
+            </Stack>
         </div>
     )
 }
