8e9fac410422ac620e3ac6d3eace3fee35e2386f
Niamh Carter <niamh.carter13@gmail.com>
2025-03-05 20:38:47 +0000

    Added a button to all projects

--- Portfolio/src/Home/HeroSection.js
+++ Portfolio/src/Home/HeroSection.js
@@ -1,5 +1,6 @@
 import './HeroSection.css';
 import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
 
 export default function HeroSection() {
     return (
@@ -8,6 +9,7 @@ export default function HeroSection() {
             <div id="title">
                 <h1 id="bigHeader">Hi, I'm Niamh - A Software Developer</h1>
                 <h3>Building .NET and native mobile applications</h3>
+                <Button id="button" href="all-projects" size="lg">View Projects</Button>
             </div>
             <img src="HeroSectionImage.png" />
             </Stack>
